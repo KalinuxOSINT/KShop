51d46c6818d0c6ccf82d971a8225f4c92be3464b
KalinuxOSINT <kalinux.osint@atomicmail.io>
2026-05-31 02:20:54 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,10 @@
-// Service Worker pour KaliNet
 const CACHE_NAME = 'kalinet-v1';
-const urlsToCache = [
-    '/',
-    '/manifest.json'
-];
+const urlsToCache = ['/', '/manifest.json'];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
